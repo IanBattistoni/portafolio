1f1eae5c2b9f42059259ddf6dff9de3a026ea1a0
unknown <ian.battistoni.f@gmail.com>
2025-05-21 01:12:16 -0400

    home terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home.js'; 
+import Home from './pages/home/home.js';
+import About from './pages/about/about.js'; 
 function App() {
   return (
     <Router>
       <Routes>
 
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
 
         <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
       </Routes>
