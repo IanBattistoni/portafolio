242536fc28c49b5b700689d9b396af5950cd99ae
unknown <ian.battistoni.f@gmail.com>
2025-05-28 01:31:42 -0400

    header sticky

--- src/shared/layouts/header/header.js
+++ src/shared/layouts/header/header.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './header.css';
 
 function Header() {
+  const [
+    scrolled, setScrolled
+  ] = useState(false)
+  useEffect(()=>{
+    const handleScroll = () => {
+      setScrolled(window.scrollY >20);
+    };
+
+    window.addEventListener('scroll',handleScroll);
+    return() => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <header className="header">
-      <div className='container-header'>
+      <nav className={`container-header ${scrolled ? 'scrolled' : ''}`}>
         <a href='#projects'  title='Ir a proyectos'>Proyectos</a>
         <a href='#experiences' title='Ir al inicio'>Experiencia</a>
         <a href='#stack' title='Sobre nosotros'>Sobre mí</a>
-      </div>
+      </nav>
     </header>
   );
 }
